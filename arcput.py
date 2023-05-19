283dcd6c947fcbaad3764ea619ea5efbf7bbb6f5
root <root@rocky.hpc.usc.edu>
2023-05-19 14:50:08 -0400

    add

---
+++ arcput.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+from redis import Redis
+from pymongo import MongoClient
+import os
+import sys
+import traceback
+
+# check argument, print usage
+if len(sys.argv) > 1:
+    filename=sys.argv[1]
+    fullpath=os.path.abspath(filename)
+else:
+    print("Error: Please input file name")
+    quit()
+
+# check if abspath contains space
+if ' ' in fullpath:
+    print("Error: File full path contains space!")
+    quit()
+
+# validate file
+ost=os.stat(fullpath)
+uid=ost.st_uid
+gid=ost.st_gid
+
+# verify ownership
+if uid != os.getuid():
+    print("Error: "+fullpath+" is not owned by you")
+    quit()
+
+# check if objname already in arcdb
+
+# check if over group quota
+
+# send abspath to putdb redis dataset
+
+# message wait
