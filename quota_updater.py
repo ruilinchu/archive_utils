c358a5b1a9f83f43e5f388c7388fbe380831e2b3
root <root@rocky.hpc.usc.edu>
2023-05-23 23:53:54 -0400

    add

--- quota_updater.py
+++ quota_updater.py
@@ -6,4 +6,18 @@ from redis import Redis
 from pymongo import MongoClient
 
 m=MongoClient("mongodb://phobos:phobos@127.0.0.1/arcdb")
-r=Redis(host='127.0.0.1')
+r=Redis(host='127.0.0.1',password='aabbccddeeffgg')
+
+# start while true sleep loop
+
+while True:
+    gido=r.spop("arcgid")
+    if gido is None:
+        break
+    else
+        gid=gido.decode("utf-8")
+        NumFile=list(m.arcdb.obj.aggregate([{"$match":{"gid":gid}},{"$group": {"_id":"$gid", "NumFile":{"$sum":1}}}]))[0]['NumFile']
+        Size=list(m.arcdb.obj.aggregate([{"$match":{"gid":gid}},{"$group": {"_id":"$gid", "Size":{"$sum":"$size"}}}]))[0]['Size']
+
+
+#end while true loop
